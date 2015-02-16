05b430842fa9760bd5f7992e668633efc07017f9
Kendyl <lemahie3@msu.edu>
2015-02-16 11:46:23 -0500

    cat pictures added

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 # app.rb
 require 'sinatra'
+require 'cat_api'
 
 class MyWebApp < Sinatra::Base
 	get '/' do
   		erb :"index#{ rand(3) + 1 }"
 	end
+end
+
+class CatIndustriesApp < Sinatra::Base
+  get '/cats' do
+    @pictures = CatAPI.new.get_images(results_per_page: 100, category: 'hats')
+    erb :cats
+  end
 end
