d9a233e39192087e67c52e9e24ef7855e4a2a2b6
Kendyl <lemahie3@msu.edu>
2015-02-25 12:09:45 -0500

    indentation changes

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require 'sinatra'
 require 'holidapi'
 
 class MyWebApp < Sinatra::Base
-	get '/' do
-		params['year'] ||= Time.now.year
-		params['month'] ||= Time.now.month
-		params['day'] ||= Time.now.day
-		if params['day'].nil? || params['day'] == ""
-			@holidays = HolidApi.get(country: 'us', year: params['year'], month: params['month']).flatten
-		else
-			@holidays = HolidApi.get(country: 'us', year: params['year'], month: params['month'], day: params['day'])
-		end
-		erb :index
-	end
+  get '/' do
+    params['year'] ||= Time.now.year
+    params['month'] ||= Time.now.month
+    params['day'] ||= Time.now.day
+    if params['day'].nil? || params['day'] == ""
+      @holidays = HolidApi.get(country: 'us', year: params['year'], month: params['month']).flatten
+    else
+      @holidays = HolidApi.get(country: 'us', year: params['year'], month: params['month'], day: params['day'])
+    end
+    erb :index
+  end
 end
